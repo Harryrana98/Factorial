92d99b02af51cff2110190b208adee26f81e55ba
Harryrana98 <ranaharendra77@gmail.com>
2025-04-04 16:00:04 +0530

    start

---
+++ script.js
@@ -0,0 +1,20 @@
+const input = document.querySelector("#input");
+const btn = document.querySelector("#btn");
+const contentDiv = document.querySelector("#content");
+// const input=document.querySelector("#input")
+
+let arr = [];
+
+btn.addEventListener("click", function () {
+    const inputNumber = input.value;
+    for (let i = inputNumber; i >= 1; i--) {
+        arr.push(i);
+    }
+    
+    const ArrValue = arr.reduce((acc, fc) => acc * fc);
+    console.log(arr);
+    
+    contentDiv.innerText="" 
+    contentDiv.innerText = ArrValue;
+  input.value = "";
+});
